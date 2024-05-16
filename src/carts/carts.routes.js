cb7d437b032dff2a19a48c0186ec4fc91f619c69
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-05-16 14:32:14 -0300

    add auths

--- src/carts/carts.routes.js
+++ src/carts/carts.routes.js
@@ -1,19 +1,61 @@
+import { authorization } from "../middlewares/authorization.js";
+import { authentication } from "../middlewares/authencations.js	";
 import { controllersCarts } from "./carts.controller.js";
 import { Router } from "express";
 const router = Router();
 
 // ALL
-router.get("/", controllersCarts.getAllCarts);
-router.post("/", controllersCarts.postCreateCart);
+router.get(
+	"/",
+	authentication,
+	authorization(["CLIENT", "USER", "ADMIN"]),
+	controllersCarts.getAllCarts
+);
+router.post(
+	"/",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.postCreateCart
+);
 
 // ID CART
-router.get("/:cid", controllersCarts.getCartById);
-router.put("/:cid", controllersCarts.putUpdateCart);
-router.delete("/:cid", controllersCarts.deleteCart);
+router.get(
+	"/:cid",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.getCartById
+);
+router.put(
+	"/:cid",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.putUpdateCart
+);
+router.delete(
+	"/:cid",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.deleteCart
+);
 
 // ID CART - ID PRODUCT
-router.post("/:cid/productos/:pid", controllersCarts.postAddProductToCart);
-router.put("/:cid/productos/:pid", controllersCarts.putUpdateProductInCart);
-router.delete("/:cid/productos/:pid", controllersCarts.deleteProductFromCart);
+router.post(
+	"/:cid/productos/:pid",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.postAddProductToCart
+);
+router.put(
+	"/:cid/productos/:pid",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.putUpdateProductInCart
+);
+router.delete(
+	"/:cid/productos/:pid",
+	authentication,
+	authorization(["CLIENT"]),
+	controllersCarts.deleteProductFromCart
+);
 
 export default router;
