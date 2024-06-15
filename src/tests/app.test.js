f44e9d2e3b78808db01a82d6032651bf5e8db161
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-06-15 18:41:20 -0300

    testing

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,17 +1,17 @@
 import supertest from "supertest";
-import mongoose from "mongoose";
 import { expect } from "chai";
+import { Users } from "../feature/users/users.schema.js";
+import Products from "../feature/products/products.schema.js";
 import { mockuser } from "./mock/user.mock.js";
 import { mockprod } from "./mock/prod.mock.js";
 import { SESSIONS_TESTS } from "./sessions.test.js";
 import { PRODUCTS_TESTS } from "./products.test.js";
 
-const db = mongoose.connect("mongodb://localhost:27017/ecommerce");
 const http = supertest("http://localhost:8080");
 
 describe("TEST API", () => {
 	before(async () => {
-		console.log("Conectando a la base de datos");
+		Users.deleteOne({ email: mockuser.userregister.email }).exec();
 	});
 
 	SESSIONS_TESTS(http, expect, mockuser);
