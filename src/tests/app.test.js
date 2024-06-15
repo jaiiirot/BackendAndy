70fe489dcc30bb07e2d1f8926fcbdda188bdc46e
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-06-15 18:19:31 -0300

    testing

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,8 +1,10 @@
-import { expect } from "chai";
 import supertest from "supertest";
-import { mockuser } from "./mock/user.mock.js";
 import mongoose from "mongoose";
+import { expect } from "chai";
+import { mockuser } from "./mock/user.mock.js";
+import { mockprod } from "./mock/prod.mock.js";
 import { SESSIONS_TESTS } from "./sessions.test.js";
+import { PRODUCTS_TESTS } from "./products.test.js";
 
 const db = mongoose.connect("mongodb://localhost:27017/ecommerce");
 const http = supertest("http://localhost:8080");
